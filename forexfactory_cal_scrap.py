96d77fb23d3ce7e7eaad4dba981148e85807080e
toufu <Terrypang89@hotmail.com>
2023-09-09 11:26:21 +0800

    [update]: update pandas dataframe for forex factory fx calender scraper

--- forexfactory_cal_scrap.py
+++ forexfactory_cal_scrap.py
@@ -4,25 +4,43 @@ from typing import List
 import urllib.request
 import urllib.parse
 import ssl
-import json
-from json import JSONEncoder
 from pytz import timezone
 import time
+from pprint import pprint
+import threading as th
+import logging
+from time import sleep
+import pandas as pd
 
-class PyEcoElement(object):
-    def __init__(self, date: str, currency: str, event: str, impact : str, time_utc: str, actual: str, forecast: str, previous: str):
-        self.date = date
-        self.currency = currency
-        self.event = event
-        self.impact = impact
-        self.time_utc = time_utc
-        self.actual = actual
-        self.forecast = forecast
-        self.previous = previous
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.num = 0
+        self.start()
 
-class PyEcoRoot(object):
-    def __init__(self, eco_elements : List[PyEcoElement]):
-        self.eco_elements = eco_elements
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+        print("done thread " + str(self.num))
+
+    def start(self):
+        if not self.is_running:
+            self._timer = th.Timer(self.interval, self._run)
+            self._timer.name = self.num
+            print("thread id=" + self._timer.name)
+            self._timer.start()
+            self.is_running = True
+            self.num = self.num + 1
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
 
 class PyEcoCal:
     def GetEconomicCalendar(self, query_date: datetime):
@@ -39,8 +57,7 @@ class PyEcoCal:
 
         # get the page and make the soup
         urleco = f"{base_url}calendar?day={query_date.strftime('%b').lower()}{query_date.day}.{query_date.year}"
-        print(f"urleco={urleco}")
-        # urleco = "https://www.forexfactory.com/calendar?day=sep1.2023"
+
         date_string = query_date.strftime('%Y-%m-%d')
         opener = urllib.request.build_opener()
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
@@ -54,23 +71,29 @@ class PyEcoCal:
 
         impact_dict = {
             "red": "high",
-            "ora": "mid",
+            "ora": "medium",
             "yel": "low",
-            "gra": "holiday"
+            "gra": "None"
         }
         
         for item in table:
             dict = {
                 "Date": "",
+                "Time": "",
                 "Currency": "",
                 "Event": "",
-                "Time_UTC": "",
                 "Impact": "",
                 "Actual": "",
+                "Pending": "",
+                "Result": "",
                 "Forecast": "",
-                "Previous": ""
+                "Previous": "",
+                "upnext": ""
             }
 
+            # check_id = item,find_all("td", {"class": ""})
+
+            # print(f"item = {item}")
             check_date = item.find_all("td", {"class": "calendar__cell"})
             if len(check_date) != 0:
                 check_exact_date =  check_date[0].text.strip()
@@ -80,6 +103,19 @@ class PyEcoCal:
                     dict["Date"] = check_exact_date
                 date_prev = dict["Date"]
             
+            check_time = item.find_all("td", {"class": "calendar__cell calendar__time"})
+            if len(check_time) != 0:
+                time_eastern = check_time[0].text.strip()
+                print(f"check_time[0]={check_time[0]}, date_string = {date_string}, time_eastern={time_eastern}")
+                if time_eastern == "All Day" or time_eastern == "Tentative" or time_eastern == "Day 1" or time_eastern == "Day 2":
+                    dict["Time"] = time_eastern
+                elif time_eastern == "":
+                    dict["Time"] = prev_time
+                else:
+                    datetime_eastern = datetime.strptime(f"{date_string} {time_eastern}", '%Y-%m-%d %I:%M%p')
+                    dict["Time"] = datetime_eastern
+                    prev_time = dict["Time"]
+
             check_currency = item.find_all("td", {"class": "calendar__cell calendar__currency"})
             if len(check_currency) != 0:
                 dict["Currency"] = check_currency[0].text.strip()  # Currency
@@ -87,21 +123,13 @@ class PyEcoCal:
             check_event= item.find_all("span", {"class": "calendar__event-title"})
             if len(check_event) != 0:
                 dict["Event"] = check_event[0].text.strip()  # Event Name
-            
-            check_time = item.find_all("td", {"class": "calendar__cell calendar__time"})
-            if len(check_time) != 0:
-                time_eastern = check_time[0].text.strip()
-                if time_eastern == "All Day" or time_eastern == "Tentative":
-                    dict["Time_UTC"] = time_eastern
-                elif time_eastern == "":
-                    dict["Time_UTC"] = prev_time
-                else:
-                    datetime_eastern = datetime.strptime(f"{date_string} {time_eastern}", '%Y-%m-%d %I:%M%p')
-                    eastern_tz = timezone('US/Eastern')
-                    dict["Time_UTC"] = eastern_tz.localize(datetime(datetime_eastern.year, datetime_eastern.month, \
-                                                                datetime_eastern.day, datetime_eastern.hour, \
-                                                                datetime_eastern.minute, 0)).strftime("%Y%m%d %H:%M:%S")
-                    prev_time = dict["Time_UTC"]
+
+            check_upnext = item.find_all('span', {"class": "icon icon--upnext"})
+            if len(check_upnext) != 0:
+                upnext_val = check_upnext[0]['class']
+                if len(upnext_val) > 1:
+                    dict["upnext"] = upnext_val[1]
+            # print(f"check_upnext = {dict['upnext']} \n")
 
             check_impact = item.find_all("td", {"class": "calendar__cell calendar__impact"})
             if len(check_impact) != 0:
@@ -119,6 +147,19 @@ class PyEcoCal:
                     actual_value = check_actual[0].span.text.strip()
                 dict["Actual"] = actual_value
 
+            check_pending =item.find_all("div", {"class": "calendar__actual-wait"})
+            if len(check_pending) != 0:
+                actual_value = check_pending[0].text.strip()
+                dict["Pending"] = actual_value
+            
+            check_result = item.select("span.worse")
+            if not check_result:
+                check_result = item.select("span.better")
+            if len(check_result) > 0:
+                filter_result = check_result[0]["class"]
+                if len(filter_result) == 1:
+                    dict['Result'] = filter_result[0]
+
             check_forecast = item.find_all("td", {"class": "calendar__cell calendar__forecast"})
             if len(check_forecast) != 0:
                 dict["Forecast"] = check_forecast[0].text.strip()
@@ -127,37 +168,66 @@ class PyEcoCal:
             if len(check_previous) != 0:
                 dict["Previous"] = check_previous[0].text.strip()
 
+            # check_snap = item.find_all("snap", {"class": "icon icon--upnext"})
+            # if len(check_snap) != 0:
+            #     dict["snap"] = check_snap[0].text.strip()
+            
+
             eco_day.append(dict)
 
         events_array = []
-
         for row_dict in eco_day:
-            eco_elem = PyEcoElement(
-                row_dict["Date"],
-                row_dict["Currency"],
-                row_dict["Event"],
-                row_dict["Impact"],
-                row_dict["Time_UTC"],
-                row_dict["Actual"],
-                row_dict["Forecast"],
-                row_dict["Previous"]
+            if not row_dict["Time"]:
+                continue 
+            events_array.append(
+                {
+                    "date": row_dict["Date"],
+                    "time": row_dict["Time"],
+                    "currency": row_dict["Currency"],
+                    "importance": row_dict["Impact"],
+                    "event": row_dict["Event"],
+                    "actual": row_dict["Actual"],
+                    "pending": row_dict["Pending"],
+                    "result": row_dict["Result"],
+                    "forecast": row_dict["Forecast"],
+                    "previous": row_dict["Previous"],
+                    "upnext": row_dict["upnext"]
+                }
             )
-            events_array.append(eco_elem)
 
-        eco_cal = PyEcoRoot(events_array)
+        return pd.DataFrame(events_array)
 
-        json_object = json.dumps(eco_cal.__dict__, default=lambda o: o.__dict__, indent=3)
-        return json_object
+def setLogger():
+    logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - \n %(message)s',
+                    filename='logs_file',
+                    filemode='w')
+    console = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - \n %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+
+
+def run_main():
+    tic = time.perf_counter()
+    setLogger()
+    eco = PyEcoCal()
+    
+    jsons = eco.GetEconomicCalendar(datetime.today())
+    # data.head()
+    logging.info(jsons.to_string())
+    toc = time.perf_counter()
+    print(f"Downloaded the tutorial in {toc - tic:0.4f} seconds")
+
 
 if __name__ == "__main__":
     """
     Run this using the command "python `script_name`.py >> `output_name`.csv"
     """
-    tic = time.perf_counter()
-    eco = PyEcoCal()
-    
-    json = eco.GetEconomicCalendar(datetime.today())
-    print(json)
+    print("starting...")
 
-    toc = time.perf_counter()
-    print(f"Downloaded the tutorial in {toc - tic:0.4f} seconds")
+    rt = RepeatedTimer(1, run_main) # it auto-starts, no need of rt.start()
+    try:
+        sleep(1000) # your long-running job goes here...
+    finally:
+        rt.stop() # better in a try/finally block to make sure the program ends!
